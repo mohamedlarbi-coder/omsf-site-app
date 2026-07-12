b5731901d0edb855f56a47a96eef808f477b9e96
mohamedlarbi-coder <mohamedlarbi.jabri@gmail.com>
2026-07-12 17:21:52 -0400

    : Use real logo image, brighter watermark

--- src/components/BackgroundWatermark.jsx
+++ src/components/BackgroundWatermark.jsx
@@ -11,7 +11,7 @@ export default function BackgroundWatermark() {
       style={{ zIndex: 0 }}
       aria-hidden="true"
     >
-      <div style={{ opacity: 0.06, transform: "scale(2.2)" }}>
+      <div style={{ opacity: 0.16, transform: "scale(2.2)" }}>
         <SentiQLogo size={360} />
       </div>
     </div>
