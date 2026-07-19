6caf352c0cb763a973edd3f04c154d54938485d3
mohamedlarbi-coder <mohamedlarbi.jabri@gmail.com>
2026-07-19 10:38:49 -0400

    Revert "Add files via upload"
    This reverts commit 9c3bdcc19a55d1a50695e3934325c8836cd4c6ee.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Inter"', "-apple-system", "BlinkMacSystemFont", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-};
+}
